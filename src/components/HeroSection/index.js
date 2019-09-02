2fcfb4c41bd34ed9c485da92b647b9fc061e878c
dburandt <behringer.19@gmail.com>
2019-09-02 15:09:01 -0400

    [feat] add slack button

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -15,13 +15,15 @@ function HeroSection(props) {
           size={1}
         />
         <div className="buttons is-centered">
-          <SectionButton
-            parentColor={props.color}
-            size="medium"
-            onClick={props.buttonOnClick}
-          >
-            {props.buttonText}
-          </SectionButton>
+          <a href="https://slack.com/oauth/authorize?client_id=579436804787.687842990625&scope=bot,incoming-webhook,files:read,commands">
+            <img
+              alt="Add to Slack"
+              height="40"
+              width="139"
+              src="https://platform.slack-edge.com/img/add_to_slack.png"
+              srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+            />
+          </a>
         </div>
       </div>
     </Section>
